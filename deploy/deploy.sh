e6dcaf34354692d8f1170a1e7fe262ef445629b4
Lee TaeWoo <syr@meu.works>
2022-02-21 02:17:56 +0900

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,18 +4,22 @@ GIT=$3
 
 mkdir -p service
 
-if [ -d "/service/$SERVICE" ]; then
+if [ -d "service/$SERVICE" ]; then
     cd service/$SERVICE
     git pull
 else
     git clone $GIT service/$SERVICE
     cd service/$SERVICE
+fi
 
-    if [ $STABLE = "true" ]; then
-        git chekcout release
-    fi
+if [ $STABLE = "true" ]; then
+    git checkout release
+else
+    git checkout master
 fi
 
 ./deploy.sh
 
-cd ../../
+if [ $? -ne 0 ]; then
+    exit 1
+fi
