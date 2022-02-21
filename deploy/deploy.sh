fd4c4f7561fe7034486dcb2e11de7e0f20a910d7
syrflover <syr@meu.works>
2022-02-21 06:55:45 +0000

    update scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,24 +1,34 @@
-SERVICE=$1
-STABLE=$2
+SVC=$1
+BRANCH=$2
 GIT=$3
+UPDATE=$4
 
 mkdir -p service
 
-if [ -d "service/$SERVICE" ]; then
-    cd service/$SERVICE
-    git pull
+if [ -d "service/$SVC" ]; then
+    cd service/$SVC
 else
-    git clone $GIT service/$SERVICE
-    cd service/$SERVICE
+    git clone $GIT service/$SVC
+    cd service/$SVC
 fi
 
-if [ $STABLE = "true" ]; then
-    git checkout release
+if [ "$BRANCH" = "stable" ]; then
+    git checkout stable
+elif [ "$BRANCH" = "beta" ]; then
+    git checkout beta
 else
-    git checkout master
+    if [ "$SVC" != "library" ] && [ "$BRANCH" = "master" ]; then
+        echo "$BRANCH is not a valid branch"
+        exit 1
+    fi
 fi
 
-./deploy.sh
+if [ $? -ne 0 ] && [ "$SVC" != "library" ]; then
+    exit 1
+fi
+
+#           is_minikube / update_flag
+./deploy.sh true $UPDATE
 
 if [ $? -ne 0 ]; then
     exit 1
--- deploy/stable.sh
+++ deploy/stable.sh
@@ -1,22 +1,36 @@
-./config.sh
+./deploy/config.sh
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./deploy.sh auth false git@github.com:Project-Madome/auth.madome.app.git
+SPECIFIED_SVC=$1
+
+if [ -n "$SPECIFIED_SVC" ]; then
+    if [ "$SPECIFIED_SVC" = "user" ]; then
+        ./deploy/deploy.sh user stable git@github.com:Project-Madome/user.madome.app.git
+    elif [ "$SPECIFIED_SVC" = "auth" ]; then
+        ./deploy/deploy.sh auth stable git@github.com:Project-Madome/auth.madome.app.git
+    elif [ "$SPECIFIED_SVC" = "library" ]; then
+        ./deploy/deploy.sh library master git@git.meu.works:madome/servers/library.git
+    fi
+
+    exit 1
+fi
+
+./deploy/deploy.sh auth stable git@github.com:Project-Madome/auth.madome.app.git
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./deploy.sh user false git@github.com:Project-Madome/user.madome.app.git
+./deploy/deploy.sh user stable git@github.com:Project-Madome/user.madome.app.git
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./deploy.sh library false git@git.meu.works:madome/servers/library.git
+./deploy/deploy.sh library master git@git.meu.works:madome/servers/library.git
 
 if [ $? -ne 0 ]; then
     exit 1
@@ -24,11 +38,11 @@ fi
 
 # ingress-nginx
 if [ -z "$(kubectl get deploy --namespace=ingress-nginx -o=name)" ]; then
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+    # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
 
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
+    echo "read https://kubernetes.github.io/ingress-nginx/deploy/#installation-guide"
+
+    exit 1
 fi
 
 kubectl apply -f ./ingress
